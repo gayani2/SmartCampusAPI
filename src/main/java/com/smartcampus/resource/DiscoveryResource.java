8191e8061f483f79ddec3ee16f645419b16a23c4
gayani2 <gayani.20230004@iit.ac.lk>
2026-04-15 12:04:34 +0530

    Initial commit - Part 1 Setup and Discovery Endpoint

---
+++ src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -0,0 +1,31 @@
+package com.smartcampus.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/")
+public class DiscoveryResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response discover() {
+
+        Map<String, String> resources = new HashMap<>();
+        resources.put("rooms",   "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("api",         "Smart Campus API");
+        response.put("version",     "1.0");
+        response.put("contact",     "admin@smartcampus.ac.uk");
+        response.put("description", "Sensor and Room Management REST API");
+        response.put("resources",   resources);
+
+        return Response.ok(response).build();
+    }
+}
